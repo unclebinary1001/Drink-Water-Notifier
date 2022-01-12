fd0019c4aa9eda2f801841f6359ed411c5159c5a
Mahlangu Nzunda <nzundamahlangu@gmail.com>
2022-01-12 02:20:49 -0600

    complete app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Drink-Water-Notifier
+This is an app that serves as a reminder to keep hydrated while working on your computer
+
+Once it's executed, the app will display a drink water notification after 30 minutes,
+and then an hour later for 2 times
+and then two hours
---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+from plyer import notification
+
+if __name__ == '__main__':
+    time.sleep(30 * 60)
+    run = 0
+
+    while run < 3:
+        notification.notify(
+            title="Drink Water",
+            message="Drink at least a glass of water\nRemember your body needs fuel",
+            timeout=30
+        )
+
+        time.sleep(3 * 60)
+        run += 1
+
